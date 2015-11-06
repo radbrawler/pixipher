9a1e8743185b15a53c78806af2289f8c57da42cd
anmol <sanmol38@gmail.com>
2015-11-06 23:40:04 +0000

    GUI almost done ... TODO's are encrypted image, metrics and histogram

--- broadcast.py
+++ broadcast.py
@@ -8,3 +8,4 @@ class Server:
     def open_connection(self):
         serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         serverSocket.bind((self.serverName, self.serverPort))
+        serverSocket.listen(5)
