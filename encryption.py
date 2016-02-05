26461b1f67dbd7bbb4cb68b744628393f8a63e5a
anmol <sanmol38@gmail.com>
2016-02-06 02:18:46 +0530

    atleast was able to scramble this time ..!!

--- encryption.py
+++ encryption.py
@@ -1,6 +1,7 @@
 import hashlib
 
 from PIL import Image
+import numpy as np
 
 
 class Encryption:
@@ -20,38 +21,37 @@ class Encryption:
                 im2 = image.crop((0, 0, 200, 200))
                 im2.show()
                 pixels = self.encrypter(im2, im2.size, image.mode)
-
-                # for i in range(image.size[0]):
-                #     for j in range(image.size[1]):
-                #         # print(i, j, end='')
-                #         pixels[i, j] = (256-pixels[i, j]) % 256
-                #         # print(pixels)
-                #     # print(i)
-
                 image_out.paste(pixels)
                 im2.save(self.outfile, 'png')
                 im2.show()
+
             elif image.mode == 'RGB':
                 im_rgb = image.crop((0, 0, 100, 100))
                 im_rgb.show()
                 red, green, blue = self.encrypter(im_rgb, im_rgb.size, im_rgb.mode)
+                print(type(im_rgb))
                 im_rgb = Image.merge(im_rgb.mode, [red, green, blue])
                 im_rgb.save(self.outfile, 'png')
                 im_rgb.show()
+
             else:
                 print("Image is neither \'L\' or \'RGB\'")
 
         except IOError as e:
             print("File Name not Found", e)
 
-    @staticmethod
-    def encrypter(c_image, size, mode):  # c_image->cropped image
+    def encrypter(self, c_image, size, mode):  # c_image->cropped image
         list1 = c_image.load()
+        p, q = 1, 1
+        arnold = ([1, p], [q, p*q+1])
+
         if mode == 'L':
             try:
                 for x in range(size[0]):
                     for y in range(size[1]):
+                        x2 = np.dot([x, y], arnold) % size[0]
                         list1[x, y] += 100
+
                 return list1
 
             except TypeError:
@@ -59,15 +59,21 @@ class Encryption:
 
         elif mode == 'RGB':
             try:
-                red, green, blue = c_image.split()
+                red1, green1, blue1 = red, green, blue = c_image.split()
                 r = red.load()
                 g = green.load()
                 b = blue.load()
+                r1 = red1.load()
+                g1 = green1.load()
+                b1 = blue1.load()
+
                 for x in range(size[0]):
                     for y in range(size[1]):
-                        r[x, y] = (r[x, y] + 100) % 256
-                        g[x, y] = (g[x, y] + 100) % 256
-                        b[x, y] = (b[x, y] + 100) % 256
+                        x2 = np.dot([x+1, y+1], arnold) % size[0]
+                        r1[x, y] = r[0, 0]
+                        r1[x, y] = r[int(x2[0]), int(x2[1])]
+                        g1[x, y] = g[int(x2[0]), int(x2[1])]
+                        b1[x, y] = b[int(x2[0]), int(x2[1])]
                 return red, green, blue
 
             except SystemError:
